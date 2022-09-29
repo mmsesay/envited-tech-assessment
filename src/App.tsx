c22603aac15f162af46403f47b2aa5c9b12fece1
Muhammad Mustapha Sesay <muhammadsesay8@gmail.com>
2022-09-29 17:06:52 +0000

    Add project structure and setup tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Hello world!fsadjfdsa</h1>
+    </div>
+  );
+}
+
+export default App;
